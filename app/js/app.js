4572c8d13e78ad685473ff2adf067c07c83fc4b5
sergey <serejatoje@gmail.com>
2021-06-20 21:01:57 +0700

    team page

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,9 @@
-import IMask from 'imask';
-
-const phoneMask = document.querySelectorAll('[type="tel"]');
-phoneMask.forEach(element => IMask(element, {
-    mask: '+{7}(000)000-00-00'
-}));
+// import IMask from 'imask';
+//
+// const phoneMask = document.querySelectorAll('[type="tel"]');
+// phoneMask.forEach(element => IMask(element, {
+//     mask: '+{7}(000)000-00-00'
+// }));
 
 document.addEventListener('DOMContentLoaded', () => {
 
