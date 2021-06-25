c07e3ee1851cd90fa06cdfb1cf9b9bdbc1d1d431
sergey <serejatoje@gmail.com>
2021-06-25 17:13:20 +0700

    contacts page

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@ import IMask from 'imask'
 const trainDistance = 160
 const phoneMask = document.querySelectorAll('[type="tel"]')
 phoneMask.forEach(element => IMask(element, {
-    mask: '+{7}(000)000-00-00'
+    mask: '+{7} (000) 000-00-00'
 }));
 
 function initTrainEvent() {
@@ -27,6 +27,21 @@ train.addEventListener('click', event => {
         behavior: "smooth"
     })
 })
+
+const formInputs = document.querySelectorAll('.form-input input')
+for (let i = 0; i < formInputs.length; i++) {
+    formInputs[i].addEventListener('focus', function (event) {
+        event.target.parentNode.classList.add('active')
+        event.preventDefault()
+    })
+    formInputs[i].addEventListener('blur', function (event) {
+        console.log(event.target.value)
+        if (event.target.value === '') {
+            event.target.parentNode.classList.remove('active')
+        }
+        event.preventDefault()
+    })
+}
 
 window.addEventListener('scroll', () => {
     initTrainEvent()
