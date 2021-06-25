b228d3b779d4ade4f3de740159948aa97764def4
sergey <serejatoje@gmail.com>
2021-06-25 10:36:30 +0700

    contacts page

--- app/js/app.js
+++ app/js/app.js
@@ -1,33 +1,30 @@
 import {initMap} from './map'
-const trainDistance = 160;
+import IMask from 'imask'
 
-// import IMask from 'imask';
-//
-// const phoneMask = document.querySelectorAll('[type="tel"]');
-// phoneMask.forEach(element => IMask(element, {
-//     mask: '+{7}(000)000-00-00'
-// }));
+const trainDistance = 160
+const phoneMask = document.querySelectorAll('[type="tel"]')
+phoneMask.forEach(element => IMask(element, {
+    mask: '+{7}(000)000-00-00'
+}));
 
 function initTrainEvent() {
     if (window.scrollY > trainDistance) {
-        document.querySelector('.train').classList.add('fade-in');
+        document.querySelector('.train').classList.add('fade-in')
     } else {
-        document.querySelector('.train').classList.remove('fade-in');
+        document.querySelector('.train').classList.remove('fade-in')
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     initTrainEvent()
-
     initMap()
+})
 
-    const train = document.querySelector('.train')
-
-    train.addEventListener('click', event => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        })
+const train = document.querySelector('.train')
+train.addEventListener('click', event => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
     })
 })
 
