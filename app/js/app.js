983eee99b786d5862cda3d017b36c9db1fcdfcca
sergey <serejatoje@gmail.com>
2021-07-11 17:10:42 +0700

    nalog page

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 import {initMap} from './map'
 import IMask from 'imask'
-import Swiper from "swiper";
+import Swiper from "swiper"
 import SwiperCore, {Navigation, Pagination} from 'swiper/core'
 
 SwiperCore.use([Navigation, Pagination])
@@ -8,7 +8,7 @@ SwiperCore.use([Navigation, Pagination])
 const params = {
     paginationUpdate: function (e, paginationEl) {
         const bullets = document.querySelectorAll('.blog-pagination .swiper-pagination-bullet');
-        document.querySelector('.swiper-pages').innerHTML = '';
+        document.querySelector('.swiper-pages').innerHTML = ''
         for (let i = 0; i < bullets.length; i++) {
             document.querySelector('.swiper-pages').innerHTML += '<button class="slider-button">' + (i + 1) + '</button>';
         }
@@ -23,10 +23,38 @@ const params = {
             })
         }
 
-        const pages = document.querySelectorAll('.blog .slider-button');
+        const pages = document.querySelectorAll('.blog .slider-button')
         for (let i = 0; i < bullets.length; i++) {
             if (bullets[i].hasClass('swiper-pagination-bullet-active')) {
-                pages[i].classList.add('active');
+                pages[i].classList.add('active')
+            }
+        }
+
+    }
+}
+
+const service_params = {
+    paginationUpdate: function (e, paginationEl) {
+        const bullets = document.querySelectorAll('section.services .swiper-pagination-bullet')
+        document.querySelector('section.services .swiper-pages').innerHTML = ''
+        for (let i = 0; i < bullets.length; i++) {
+            document.querySelector('section.services .swiper-pages').innerHTML += '<button class="slider-button">' + (i + 1) + '</button>'
+        }
+
+        const servicePagination = document.body.querySelectorAll('section.services .swiper-pages button')
+        for (let i = 0; i < servicePagination.length; i++) {
+            servicePagination[i].addEventListener('click', function (event) {
+                const slider = document.querySelector('.services-slider').swiper
+                const index = [...event.target.parentElement.children].indexOf(event.target)
+                slider.slideTo(index)
+                event.preventDefault()
+            })
+        }
+
+        const pages = document.querySelectorAll('section.services .slider-button')
+        for (let i = 0; i < bullets.length; i++) {
+            if (bullets[i].hasClass('swiper-pagination-bullet-active')) {
+                pages[i].classList.add('active')
             }
         }
 
@@ -61,7 +89,6 @@ train.addEventListener('click', event => {
 })
 
 function filterBlogSlider(filterName) {
-    console.info('Blog filter:', filterName)
 
     if (filterName) {
         const blogSiblingsSlides = document.querySelectorAll('.blog-slider .blog-slide:not([data-filter="' + filterName + '"])')
@@ -82,14 +109,14 @@ function filterBlogSlider(filterName) {
         }
     }
 
-    const slider = document.querySelector('.blog-slider').swiper;
+    const slider = document.querySelector('.blog-slider').swiper
     slider.destroy(true, true)
 
-    const slider_columns = document.querySelector('.blog-slider').hasClass('column-1');
-    let columns = 1;
+    const slider_columns = document.querySelector('.blog-slider').hasClass('column-1')
+    let columns = 1
 
     if (!slider_columns) {
-        columns = 2;
+        columns = 2
     }
 
     if (document.querySelectorAll('.blog-slider .swiper-slide').length > 3) {
@@ -124,7 +151,7 @@ function filterBlogSlider(filterName) {
                 nextEl: '.blog-next',
                 prevEl: '.blog-prev',
             },
-        });
+        })
     } else {
         new Swiper('.blog-slider', {
             on: params,
@@ -156,20 +183,92 @@ function filterBlogSlider(filterName) {
                 nextEl: '.blog-next',
                 prevEl: '.blog-prev',
             },
-        });
+        })
     }
-
 }
 
-const blogFilter = document.querySelectorAll('.filter a')
+function filterServiceSlider(filterName) {
+
+    if (filterName) {
+        const serviceSiblingsSlides = document.querySelectorAll('.services-slider .service-slide:not([data-filter="' + filterName + '"])')
+        for (let i = 0; i < serviceSiblingsSlides.length; i++) {
+            serviceSiblingsSlides[i].classList.add('non-swiper-slide')
+            serviceSiblingsSlides[i].classList.remove('swiper-slide')
+        }
+        const serviceSlides = document.querySelectorAll('.services-slider .service-slide[data-filter="' + filterName + '"]')
+        for (let i = 0; i < serviceSlides.length; i++) {
+            serviceSlides[i].classList.add('swiper-slide')
+            serviceSlides[i].classList.remove('non-swiper-slide')
+        }
+    } else {
+        const serviceSlides = document.querySelectorAll('.services-slider .service-slide')
+        for (let i = 0; i < serviceSlides.length; i++) {
+            serviceSlides[i].classList.add('swiper-slide')
+            serviceSlides[i].classList.remove('non-swiper-slide')
+        }
+    }
+
+    const slider = document.querySelector('.services-slider').swiper
+    slider.destroy(true, true)
+
+    new Swiper('.services-slider', {
+        on: service_params,
+        slidesPerView: 2,
+        spaceBetween: 30,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                slidesPerColumn: 2
+            },
+            1024: {
+                slidesPerView: 2,
+                slidesPerColumn: 2,
+                spaceBetween: 30,
+            },
+            1366: {
+                slidesPerView: 3,
+                slidesPerColumn: 2,
+                spaceBetween: 30,
+            }
+        },
+        slidesPerColumnFill: 'row',
+        pagination: {
+            el: '.services-slider .swiper-pagination',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.service-next',
+            prevEl: '.service-prev',
+        },
+    })
+}
+
+const blogFilter = document.querySelectorAll('.filter.blog a')
 for (let i = 0; i < blogFilter.length; i++) {
     blogFilter[i].addEventListener('click', function (event) {
+
         Array.prototype.forEach.call(blogFilter, function (el, i) {
+            el.classList.remove('active')
+        })
+
+        event.target.classList.add('active')
+
+        filterBlogSlider(event.target.getAttribute('data-category'))
+
+        event.preventDefault()
+    })
+}
+
+const serviceFilter = document.querySelectorAll('.filter.service a')
+for (let i = 0; i < serviceFilter.length; i++) {
+    serviceFilter[i].addEventListener('click', function (event) {
+        Array.prototype.forEach.call(serviceFilter, function (el, i) {
             el.classList.remove('active')
         })
         event.target.classList.add('active')
 
-        filterBlogSlider(event.target.getAttribute('data-category'))
+        filterServiceSlider(event.target.getAttribute('data-category'))
 
         event.preventDefault()
     })
